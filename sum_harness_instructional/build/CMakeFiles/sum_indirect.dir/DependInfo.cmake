7aa701550acd58c525691e2766d32273153da0a7
Cole Wadsworth Brausen <cbrausen@x1004c3s2b0n0h0.chn.perlmutter.nersc.gov>
2024-10-26 15:54:48 -0700

    perlmutter job completed

--- sum_harness_instructional/build/CMakeFiles/sum_indirect.dir/DependInfo.cmake
+++ sum_harness_instructional/build/CMakeFiles/sum_indirect.dir/DependInfo.cmake
@@ -8,15 +8,11 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/nuns/Desktop/sum_harness_instructional/sum_indirect.cpp" "CMakeFiles/sum_indirect.dir/sum_indirect.cpp.o" "gcc" "CMakeFiles/sum_indirect.dir/sum_indirect.cpp.o.d"
+  "/global/homes/c/cbrausen/harness-mips-/sum_harness_instructional/sum_indirect.cpp" "CMakeFiles/sum_indirect.dir/sum_indirect.cpp.o" "gcc" "CMakeFiles/sum_indirect.dir/sum_indirect.cpp.o.d"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
